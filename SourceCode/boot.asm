d2767291c3daefdb68ce02acbaf671ec703ca4e9
Wasgyst <ohsatanalimbrb@gmail.com>
2026-04-20 22:54:03 +0500

    Update boot.asm

--- SourceCode/boot.asm
+++ SourceCode/boot.asm
@@ -1,7 +1,31 @@
 [org 0x7c00]
+cli
 xor ax, ax
 mov cs, ax
 mov ds, ax
 mov es, ax
 mov ss, ax
 mov ax, 0x7c00
+mov sp, ax
+mov bp, ax
+sti
+mov ax, 0xb800
+mov ds, ax
+xor ax, ax
+mov di, ax
+mov ax, 0x1f00
+mov cx, 64000
+rep stosw
+xor ax, ax
+mov ds, ax
+mov di, ax
+mov si, msg
+print:
+  lodsb
+  cmp al, 0
+  je done
+  mov ah, 0x0e
+  int 0x10
+done:
+  hlt
+msg: db "Hello World!", 0
