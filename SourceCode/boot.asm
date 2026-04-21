7b4b66dbfdd0f7f2ac346a55537b6765fc7b334e
Wasgyst <ohsatanalimbrb@gmail.com>
2026-04-21 16:48:54 +0500

    Update boot.asm

--- SourceCode/boot.asm
+++ SourceCode/boot.asm
@@ -10,38 +10,21 @@ mov ax, 0x0003
 int 0x10
 mov ax, 0xb800
 mov es, ax
-xor ax, ax
-mov di, ax
+xor di, di
 mov ax, 0x1f00
 mov cx, 2000
 rep stosw
-xor ax, ax
-mov es, ax
-mov di, ax
+xor di, di
 mov si, msg
-fixESfortext:
-  mov ax, 0xb800
-  mov es, ax
-  jmp printcontinue
-fixDIfortext:
-  xor di, di
-  jmp printcontinue1
 print:
-  lodsw
+  lodsb
   cmp al, 0
   je done
-  push ax
-  mov ax, es
-  cmp ax, 0xb800
-  jne fixESfortext
-  printcontinue:
-  cmp di, 1999
-  ja fixDIfortext
-  printcontinue1:
+  mov ah, 0x1f
   stosw
   jmp print
 done:
   jmp $
-msg:db 0x1f, "H", 0x1f, "e", 0x1f, "l", 0x1f, "l", 0x1f,  "o", 0x1f, " ", 0x1f, "W", 0x1f, "o", 0x1f, "r", 0x1f, "l", 0x1f, "d", 0x1f, "!", 0, 0
+msg: db "Hello World!", 0
 times 510-($-$$) db 0
 dw 0x55AA
