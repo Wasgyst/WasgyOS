021ad61529cf04ef32540700cb5a199d2adfc286
Wasgyst <ohsatanalimbrb@gmail.com>
2026-04-24 14:37:57 +0500

    Add files via upload

--- SourceCode/boot.asm
+++ SourceCode/boot.asm
@@ -6,6 +6,7 @@ mov es, ax
 mov ss, ax
 mov sp, 0x7c00
 sti
+PCPoint: dw 0x0000
 mov ax, 0x0003
 int 0x10
 mov ax, 0xb800
@@ -15,6 +16,7 @@ mov ax, 0x1f00
 mov cx, 2000
 rep stosw
 xor di, di
+mov word [PCPoint], continue1
 mov si, msg
 print:
   lodsb
@@ -24,7 +26,22 @@ print:
   int 0x10
   jmp print
 done:
-  jmp $
+  jmp PCPoint
 msg: db "WasgyOS Setup", 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 13, "PRESS ENTER TO CONTINUE OR ESC TO EXIT", 0
+keywait:
+mov ah, 0x00
+int 0x16
+ret
+continue1:
+call keywait
+cmp al, 0xD
+je next
+cmp al, 0x1B
+je exit
+jmp continue1
+exit:
+jmp 0xFFFF:0x0000
+next:
+hlt
 times 510-($-$$) db 0
 dw 0x55AA
